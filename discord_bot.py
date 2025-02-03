28399ea6c0eaf24de0346152770e40f5015830c8
arjunjoshua <arjunjoshua@gmail.com>
2025-02-03 11:48:49 +0000

    Updated the discord bot to allow it to respond to commands

--- discord_bot.py
+++ discord_bot.py
@@ -1,26 +1,36 @@
 #!/usr/bin/env python3
-
 import discord
 import datetime
 from main import get_shrine_from_nightlight
 from dotenv import load_dotenv
 import os
+from discord.ext import commands
+import asyncio
 
 load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
-
 shrine_channel_id = os.getenv("SHRINE_CHANNEL_ID")
 
 # Create a discord client
 intents = discord.Intents.default()
 intents.messages = True
-client = discord.Client(intents=intents)
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
 
+    # # get the shrine of secrets
+    # shrine_perks = get_shrine_from_nightlight()
+    #
+    # # send the shrine of secrets to the channel
+    # await send_to_channel(shrine_perks)
+
+
+@bot.command()
+async def shrine(ctx: commands.Context):
     # get the shrine of secrets
     shrine_perks = get_shrine_from_nightlight()
 
@@ -30,7 +40,7 @@ async def on_ready():
 
 # send data to a channel
 async def send_to_channel(shrine_of_secrets):
-    channel = client.get_channel(int(shrine_channel_id))
+    channel = bot.get_channel(int(shrine_channel_id))
     date = datetime.datetime.now()
 
     # Get the next Tuesday's date
@@ -39,15 +49,39 @@ async def send_to_channel(shrine_of_secrets):
 
     shrine_refresh_date = date.strftime("%b %d, %Y") + " 16:00 UTC"
 
-    formatted_shrine = (f"Shrine of Secrets currently contains the following perks:\n\n" + "\n".join(shrine_of_secrets)
+    formatted_shrine = (f"The Shrine of Secrets currently contains the following perks:\n\n" + "\n".join(shrine_of_secrets)
                         + f"\n\nAvailable until {shrine_refresh_date}")
 
     if channel:
         await channel.send(formatted_shrine)
 
-    # close the connection
-    await client.close()
+
+async def schedule_weekly_shrine():
+    await bot.wait_until_ready()  # Wait until bot is ready
+
+    while not bot.is_closed():
+        now = datetime.datetime.utcnow()
+        target_time = now.replace(hour=16, minute=0, second=0, microsecond=0)
+
+        # Find the next Tuesday
+        days_until_tuesday = (1 - now.weekday()) % 7  # 0 = Monday, 1 = Tuesday, etc.
+        target_time += datetime.timedelta(days=days_until_tuesday)
+
+        # If it's already past 16:00 UTC today, schedule for next week
+        if now >= target_time:
+            target_time += datetime.timedelta(weeks=1)
+
+        # Calculate the wait time until the next scheduled run
+        wait_time = (target_time - now).total_seconds()
+        print(f"Next shrine update scheduled for: {target_time} UTC")
+
+        await asyncio.sleep(wait_time)  # Sleep until the target time
+
+        # Fetch and send shrine details
+        shrine_perks = await get_shrine_from_nightlight()
+        await send_to_channel(shrine_perks)
+
 
 # start the bot
-client.run(token)
+bot.run(token)
 
